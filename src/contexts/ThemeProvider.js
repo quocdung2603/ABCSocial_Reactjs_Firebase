5c89df5b012712e5ae37c20be54be94834d3b895
nvmttq <nvmttq@gmail.com>
2024-03-26 14:16:44 +0700

    add folder assets

--- src/contexts/ThemeProvider.js
+++ src/contexts/ThemeProvider.js
@@ -4,24 +4,32 @@ export const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
   const COLORS = {
-    background: "#FFF",
+    background: "#F0F2F5",
+    primary: "#0866FF",
+    secondary: "#E4E6EB",
+    text: "#050505"
   };
 
   const SIZES = {
-    sm: "16px",
-    m: "18px",
+    sm: "14px",
+    m: "16px",
     lg: "20px",
     head: "30px",
   };
 
+  const SPACING = {
+
+  }
+
   const FONTS = {
-    poppins: "Arial, sans-serif",
+    primary: "Nunito Sans, Arial, sans-serif",
   };
 
   const theme = {
     COLORS,
     SIZES,
     FONTS,
+    SPACING,
   };
   
   return (
