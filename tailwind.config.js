5857d30f0f6f0a1db271f64f6659ec09bc0bb52f
nvmttq <nvmttq@gmail.com>
2024-03-27 08:45:20 +0700

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {colors: {
+      background: "#F0F2F5",
+      primary: "#0866FF",
+      secondary: "#E4E6EB",
+      text: "#050505",
+    },},
+    
   },
   plugins: [],
-}
-
+};
